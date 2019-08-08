ae84927746c4fe2c8721841119ba9578a371c6be
Apple <apple@Apples-MacBook-Pro-79.local>
2019-08-08 15:46:46 -0400

    Complete Iteration1

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func showMessage(sender:
+        UIButton) {
+        let alertController = UIAlertController(title: "hello!", message: "yeehaw!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func disappointed(_ sender: Any) {
+        let alertController = UIAlertController(title: "you good?", message: "me too.", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
