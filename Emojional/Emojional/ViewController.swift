b82b2ea94e137762a3900fff2471f4a38198ce2c
Apple <apple@Apples-MacBook-Pro-79.local>
2019-08-09 10:13:06 -0400

    final

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -12,27 +12,21 @@ class ViewController: UIViewController {
     
     let emojis = ["😔": "disappointed", "🤠": "cowboy"]
     
-    var customMessages = ["disappointed" : "me too", "disappointed" : "sad yeehaw :(", "disappointed" : "everybody makes mistakes, everybody has those days", "cowboy" : "yeehaw!", "cowboy" : "you got this!", "cowboy" : "you're doing great!"]
+    let customMessages = ["disappointed" : ["me too", "it's okay", "everybody makes mistakes, everybody has those days"], "cowboy" : ["yeehaw!", "you got this!", "you're doing great!"]]
     
     @IBAction func showMessage(sender:
         UIButton) {
         let selectedEmotion =
         sender.titleLabel!.text
-        let alertController = UIAlertController(title: "\(emojis[selectedEmotion!]!)", message: "yeehaw!", preferredStyle: UIAlertController.Style.alert)
+        let random = Int.random(in: 0 ..< 3)
+        let emojiMessages = customMessages[emojis[selectedEmotion!]!]?[random]
+        
+        let alertController = UIAlertController(title: "\(emojis[selectedEmotion!]!)", message: "\(emojiMessages!)", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style:
             UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
     }
-    
-//    @IBAction func disappointed(_ sender: Any) {
-//        let alertController = UIAlertController(title: "you good?", message: "me too.", preferredStyle: UIAlertController.Style.alert)
-//        alertController.addAction(UIAlertAction(title: "OK", style:
-//            UIAlertAction.Style.default, handler: nil))
-//
-//        present(alertController, animated: true, completion: nil)
-//    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
